e56b07009f16a87c234e7deb6529554ddac9f7d1
Tommy Lamb <james003pond@gmail.com>
2017-12-11 23:21:49 +0000

    Initial JSON Specification

--- DB/TableCreation.sql
+++ DB/TableCreation.sql
@@ -7,6 +7,7 @@ JSON TEXT not null
 
 Create Table MenuItem (
 ItemID int(3) primary key,
+SectionID int(3) not null,
 LongName varchar(144) not null
 ShortName varchar(32) not null,
 Description TEXT not null,
@@ -17,20 +18,22 @@ Create Table Orders (
 TableNumber int(2) not null,
 OrderName varchar(32) not null,
 TimeReceived TIME not null DEFAULT NOW(),
+OrderChanged TIME not null DEFAULT NOW(),
+EstimateChanged TIME,
 ETA TIME,
 Complete BOOLEAN not null DEFAULT FALSE,
 primary key (TableNumber, OrderName)
 )ENGINE=INNODB;
 
 Create Table OrderItems (
-TableNumber int(2) not null,
+OrderNumber int(2) not null,
 OrderName varchar(32) not null,
 ItemID int(3)not null,
 Amount int(3) not null,
-Details TEXT not null, --can be the empty string, but not null
+Details varchar(144) not null, --can be the empty string, but not null
 GroupNumber int(2) not null DEFAULT 1,
 ETA TIME,
-Primary Key (TableNumber, OrderName, ItemID),
+Primary Key (TableNumber, OrderName, ItemID, Details),
 foreign key (TableNumber, OrderName) references Orders (TableNumber, OrderName) ON DELETE CASCADE,
 foreign key (ItemID) references MenuItem (ItemID)
 )ENGINE=INNODB;
